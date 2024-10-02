338696019cc6aa3a900cbcf09705793543653c6d
leaderome <jeromegalang0204@gmail.com>
2024-10-02 00:54:31 -0700

    Update index (1).php

--- KFC CONCEPCION (1)/index (1).php
+++ KFC CONCEPCION (1)/index (1).php
@@ -45,7 +45,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KFC Inventory System</title>
+    <title>KFC Inventory </title>
     <link rel="stylesheet" href="style3.css">
     <style>
         body { font-family: Arial, sans-serif; }
